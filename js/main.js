2106143c88610b215dedb8a5d23f4c8d7a7f0684
CovaliAna <121186044+CovaliAna@users.noreply.github.com>
2023-05-24 18:52:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+AOS.init({
+    duration: 800,
+    once: true
+});
+
+new SimpleLightbox({ elements: ".galerie-container a"});
+
+new SimpleLightbox({ elements: ".orar-imagini a"});
+
+emailjs.init("sLE8DO6hI5oJPOg7K");
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_cln6cmw", "template_mena7qi", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+
+}
+
